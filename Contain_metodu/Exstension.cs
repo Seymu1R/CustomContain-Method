09b19ba3f95c90be9004bd984db838f932ecc09a
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-05-04 16:52:52 +0400

    first commit

---
+++ Contain_metodu/Exstension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contain_metodu
+{
+    static class Exstension
+    {
+        public static void customContain(this string SearchItem, string FindItem)
+        {
+            
+            string shema = string.Empty;
+            bool result = false;
+
+            for (int i = 0; i < SearchItem.Length; i++)
+            {
+                if ((i + FindItem.Length) > SearchItem.Length)
+                {
+                    break;
+                }
+                shema = SearchItem.Substring(i, FindItem.Length);
+                if (shema.ToLower() == FindItem.ToLower())
+                {
+                    result = true;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Contain_metodu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Contain_metodu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word= "Bu bir custom contains methodudur";
+
+            word.customContain("TaIns");
+        }
+    }
+}
